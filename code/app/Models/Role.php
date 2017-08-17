10b7ee6c154161b537d2c9d56a3202abae6b629c
Bryce Meyer <bryce@pomeloproductions.com>
2017-08-16 20:49:10 -0500

    created roles model

---
+++ code/app/Models/Role.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Government\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * Class Role
+ * @package Government\Models
+ * @property $name The name of this role
+ * @property $users Collection All users that have this role
+ */
+class Role extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name'
+    ];
+
+    /**
+     * @return BelongsToMany
+     */
+    public function users() {
+        return $this->belongsToMany(User::class);
+    }
+}
