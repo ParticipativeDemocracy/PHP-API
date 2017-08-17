3cd63a4b87c50208ae6dac69733d65e9be6ec5e6
Bryce Meyer <bryce@pomeloproductions.com>
2017-08-16 20:49:51 -0500

    filled out user model details

--- code/app/Models/User.php
+++ code/app/Models/User.php
@@ -1,13 +1,23 @@
 <?php
 
-namespace Government;
+namespace Government\Models;
 
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+/**
+ * Class User
+ * @package Government\Models
+ * @property $name string this users full name
+ * @property $email string this users email address
+ * @property $password string This users password
+ * @property $roles Collection all roles this user is connected to
+ */
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
@@ -26,4 +36,18 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * @param $password
+     */
+    public function setPasswordAttribute ($password) {
+        $this->attributes['password'] = password_hash($password, PASSWORD_BCRYPT);
+    }
+
+    /**
+     * @return BelongsToMany
+     */
+    public function roles(): BelongsToMany {
+        return $this->belongsToMany(Role::class);
+    }
 }
