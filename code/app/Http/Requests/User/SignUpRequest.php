b6e6e334008370da0b4b87ae1c3e6991f624236f
Bryce Meyer <bryce@pomeloproductions.com>
2017-08-18 19:06:06 -0500

    added sign up request

---
+++ code/app/Http/Requests/User/SignUpRequest.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: brycemeyer
+ * Date: 8/18/17
+ * Time: 7:04 PM
+ */
+
+namespace Government\Http\Requests\User;
+
+
+use Illuminate\Foundation\Http\FormRequest;
+
+/**
+ * Class SignUpRequest
+ * @package Government\Http\Requests\User
+ */
+class SignUpRequest extends FormRequest
+{
+
+    /**
+     * @return array
+     */
+    public function rules() {
+
+        return [
+            'email' => 'required|email',
+            'password' => 'required|string|min:6',
+            'name' => 'required|string'
+        ];
+    }
+}
