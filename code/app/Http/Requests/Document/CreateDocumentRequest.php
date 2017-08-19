76e2259d4f65b7d828488f7304434bfb574c257e
Bryce Meyer <bryce@pomeloproductions.com>
2017-08-18 19:08:32 -0500

    added create document request

---
+++ code/app/Http/Requests/Document/CreateDocumentRequest.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: brycemeyer
+ * Date: 8/18/17
+ * Time: 7:07 PM
+ */
+
+namespace Government\Http\Controllers\Document;
+
+
+use Illuminate\Foundation\Http\FormRequest;
+
+/**
+ * Class CreateDocumentRequest
+ * @package Government\Http\Controllers\Document
+ */
+class CreateDocumentRequest extends FormRequest
+{
+
+    /**
+     * @return array
+     */
+    public function rules() {
+
+        return [
+            'title' => 'required|string',
+        ];
+    }
+}
