aeef34fd0a148af286087e7b3d7450e40ba04036
Bryce Meyer <bryce@pomeloproductions.com>
2017-08-19 15:37:44 -0500

    configured JWT

--- code/config/app.php
+++ code/config/app.php
@@ -177,6 +177,11 @@ return [
         Government\Providers\EventServiceProvider::class,
         Government\Providers\RouteServiceProvider::class,
 
+        /**
+         * JWT Package
+         */
+        Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class
+
     ],
 
     /*
@@ -225,6 +230,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class
 
     ],
 
