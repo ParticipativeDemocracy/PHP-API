34bf46b2fea46bbf0f786ad9c791a9d754959fc0
Bryce Meyer <bryce@pomeloproductions.com>
2017-08-18 19:04:19 -0500

    added login request

---
+++ code/app/Http/Requests/User/LoginRequest.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: brycemeyer
+ * Date: 8/18/17
+ * Time: 6:59 PM
+ */
+
+namespace Government\Http\Requests\User;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+/**
+ * Class LoginRequest
+ * @package Government\Http\Requests\User
+ */
+class LoginRequest extends FormRequest
+{
+
+    /**
+     * @return array
+     */
+    public function rules() {
+
+        return [
+            'email' => 'required|email',
+            'password' => 'required|min:6'
+        ];
+    }
+}
