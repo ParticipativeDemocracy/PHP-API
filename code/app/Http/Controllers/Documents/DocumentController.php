8214fe7f52786abec039ab0b3418691f5f8c07b8
Bryce Meyer <bryce@pomeloproductions.com>
2017-08-19 15:58:14 -0500

    created documents controller

---
+++ code/app/Http/Controllers/Documents/DocumentController.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: brycemeyer
+ * Date: 8/19/17
+ * Time: 3:45 PM
+ */
+namespace Government\Http\Controllers\Documents;
+
+use Government\Http\Controllers\Controller;
+use Government\Http\Controllers\Document\CreateDocumentRequest;
+use Government\Models\Document\Document;
+use Illuminate\Http\JsonResponse;
+
+class DocumentController extends Controller
+{
+
+    /**
+     * @var Document
+     */
+    private $document;
+
+    /**
+     * DocumentController constructor.
+     */
+    public function __construct()
+    {
+        $this->document = new Document();
+    }
+
+    /**
+     * Loads all documents in the system
+     *
+     * @return JsonResponse
+     */
+    public function all() : JsonResponse {
+
+        $documents = Document::all();
+
+        $data = [];
+
+        foreach ($documents as $document) {
+            $row = clone $document;
+            $row->content = $document->currentIteration()->content;
+            
+            $data[] = $row;
+        }
+
+        return new JsonResponse([
+            'message' => 'Successfully Loaded All Documents',
+            'documents' => $data
+        ]);
+    }
+
+    public function create(CreateDocumentRequest $request) : JsonResponse {
+
+    }
+
+}
