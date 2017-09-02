731b568cf53ea4e49b79439fdb93d8d6e1a14724
Bryce Meyer <bryce13950@gmail.com>
2017-09-02 13:35:49 -0500

    added iteration repository

---
+++ code/app/Repositories/Document/IterationRepository.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Quixotical
+ * Date: 8/20/17
+ * Time: 16:22
+ */
+namespace Government\Repositories\Document;
+
+use Government\Contracts\Repositories\Document\IterationRepositoryContract;
+use Government\Models\Document\Document;
+use Government\Models\Document\Iteration;
+
+/**
+ * Class DocumentIterationRepository
+ * @package App\Repositories
+ */
+class DocumentIterationRepository implements IterationRepositoryContract
+{
+    /**
+     * @var Iteration $DocumentIteration - the document model for this repository instance
+     */
+    protected $iteration;
+
+    /**
+     * @var string ending of foreign key fields
+     */
+    protected $id = '_id';
+
+    /**
+     * Constructor for the DocumentIterationRepository
+     * @param Iteration $iteration
+     */
+    public function __construct(Iteration $iteration){
+        $this->iteration = $iteration;
+    }
+
+    /**
+     * Create new document Iteration
+     * @param array $data
+     * @param Document $document
+     * @return $this|\Illuminate\Database\Eloquent\Model
+     * @internal param EloquentAbstractModel $parent
+     */
+    public function create(array $data = [], Document $document)
+    {
+        $tableName = $document->getTable();
+        $foreignKey = $this->createForeign($tableName);
+        $dataAndFk = array_add($data, $foreignKey, $document->id);
+        return $this->iteration->create($dataAndFk);
+    }
+
+    private function createForeign($tableName){
+        return str_singular($tableName) . $this->id;
+    }
+}
